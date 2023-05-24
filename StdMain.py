0a644457c639b015391f60132f0d6b61b19621a9
Gizem <109113608+giizzz@users.noreply.github.com>
2023-05-24 16:50:17 +0300

    oop in python

---
+++ StdMain.py
@@ -0,0 +1,7 @@
+from Student import Student
+
+std1=Student("Mike","Engineer",3.4)
+std2=Student("Beth","Accounting",3.7)
+
+print("Mike is honor roll->",std1.on_honor_roll())
+print("Lisa is honor roll->",std2.on_honor_roll())
